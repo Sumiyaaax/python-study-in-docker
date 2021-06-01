e398648b17b9dfc577a653b061ca53ca3c5e98a7
Sumiya Kurihara <wave_soul14@yahoo.co.jp>
2021-06-01 09:32:13 +0900

    20210601

---
+++ 2021/06/01/app.py
@@ -0,0 +1,11 @@
+import datetime
+# import pandas as pd
+
+# dt_now = str(datetime.datetime.now()).strftime('%Y%m%d-%H%M%S')
+
+# df = pd.DataFrame(
+#     [[dt_now, "kinocode", "Hello cron"]],
+#     columns=['datetime', 'name', 'greet']
+# )
+# df.to_csv('/Users/sumiya/Documents/MyDocker/python-study/2021/06/01/test_' + dt_now + '.csv')
+print(' Hello world from cron')
